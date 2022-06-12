d413ae85b74486c51d60302baa1b4da2d8bd48c1
ShuoWen5656 <2260755767@qq.com>
2022-06-12 20:38:22 +0800

    helloWorld提交

---
+++ Common/hello.c
@@ -0,0 +1,16 @@
+/**
+ * written by swzhao
+ * Copyright 2022
+ */
+#include <stdio.h>
+
+
+/*************************************
+ * I will success                    *
+ * swzhao                            *
+ * ***********************************/
+int main(void)
+{
+    printf("hello world!! \" zsw\" \n"); // the quotation is like java's
+    return 0;
+}
