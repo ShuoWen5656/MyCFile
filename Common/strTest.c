8994aac52c59346200de1096e1ea8d6dbecfbfe8
ShuoWen5656 <2260755767@qq.com>
2022-09-18 20:03:39 +0800

    [新增代码]取值运算符和去指运算符

--- Common/strTest.c
+++ Common/strTest.c
@@ -3,12 +3,13 @@
 //
 
 
-#define __STDC_WANT_LIB_EXT1__1
+//#define _STDC_WANT_LIB_EXT1__ 1
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <string.h>
-
-
+#include <stdbool.h>
 
 
 int main(void){
@@ -21,29 +22,69 @@ int main(void){
             "my second str"
     };
 
+    // 测试指针
+//    char a = 'a';
+//
+//    char* strp = &a;
+//
+//    printf("strp is %zu", strp);
+//    printf("strp1 is %zu", &a);
+    // gets_s测试
 
-    printf_s("this is \" %s \" \n", str);
+    char gets_str[21];
 
-    printf("len of str2 is %zu\n", sizeof(str2));
+//    if (fgets(gets_str, 21, stdin))
+//    {
+//        printf("get Str is %s", gets_str);
+//    }else
+//    {
+//        printf("Error str!");
+//    }
+
+
+    char dstr[] = "0.2 0.5 0.7";
+
+
+    // 滑动指针,存储的应该是数组中的地址
+    char* ptr = NULL;
+    char* dstrp = dstr;
+
+    while (true){
+        // 给地址的地址到函数中，函数更改地址指向的内容
+        double value = strtod(dstrp, &ptr);
+        if (ptr == dstrp){
+            break;
+        }
+        printf("%f \n", value);
+        dstrp = ptr;
+    }
+
+
+
+
+//    printf_s("this is \" %s \" \n", str);
+
+//    printf("len of str2 is %zu\n", sizeof(str2));
 
     // 计算真实的str长度
-    for (int i = 0; i < sizeof(str2)/ sizeof(str2[0]); ++i)
-    {
-        printf("str%d is \"%s\" \n", i, str2[i]);
-        printf("str%d len is %zu \n", i, strnlen(str2[i], sizeof(str2[i])));
-    }
+//    for (int i = 0; i < sizeof(str2)/ sizeof(str2[0]); ++i)
+//    {
+//        printf("str%d is \"%s\" \n", i, str2[i]);
+//        printf("str%d len is %zu \n", i, strnlen(str2[i], sizeof(str2[i])));
+//    }
+//
+//
+//    char str_cp[26];
+//    char str_temp[26];
+//    for (int i = 0; i < sizeof(str2)/ sizeof(str2[0]); ++i){
+//        strcat(str_temp, str2[i]);
+//        printf("str_temp len is %zu \n", strnlen(str_temp, sizeof(str_temp)));
+//    }
 
-
-    char str_cp[25];
-    char str_temp[25];
-    for (int i = 0; i < sizeof(str2)/ sizeof(str2[0]); ++i){
-        strcat(str_temp, str2[i]);
-        printf("str_temp len is %zu \n", strnlen(str_temp, sizeof(str_temp)));
-    }
-
-    strcpy(str_cp, str_temp);
-
-    printf("str_cp len is %zu \n", strnlen(str_cp, sizeof(str_cp)));
+//    strcpy(str_cp, str_temp);
+    // 检查\0
+//    strncpy_s(str_cp, sizeof(str_cp),str_temp, sizeof(str_temp));
+//    printf("str_cp len is %zu \n", strnlen(str_cp, sizeof(str_cp)));
 
 
 
