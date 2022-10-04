c077745cb605fa9ae24c8a8b3ccebcd3287e119c
ShuoWen5656 <2260755767@qq.com>
2022-10-04 21:05:22 +0800

    [新增代码]新增数组指针测试

--- Common/PointerTest.c
+++ Common/PointerTest.c
@@ -11,19 +11,58 @@
 int main(void){
     int *p_num = NULL, num = 3;
 
-    printf("the num  is %d \n", num);
-    // 查看地址使用%p 占位符
-    printf("the num address is %p \n", &num);
-
-    // 将地址给p_num
-    p_num = &num;
+//    printf("the num  is %d \n", num);
+//    // 查看地址使用%p 占位符
+//    printf("the num address is %p \n", &num);
+//
+//    // 将地址给p_num
+//    p_num = &num;
 
 
     // 地址是8个字节
-    printf("p_num address is %p \n", &p_num);
-    printf("p_num size is %zd\n", sizeof(p_num));
-    printf("p_num value is %p\n", p_num);
-    printf("p_num pointer value is %d\n", *p_num);
+//    printf("p_num address is %p \n", &p_num);
+//    printf("p_num size is %zd\n", sizeof(p_num));
+//    printf("p_num value is %p\n", p_num);
+//    printf("p_num pointer value is %d\n", *p_num);
+//
+//
+    // 验证数组引用和指针的关系
+    // 数组引用是一个地址，可以和指针一样计算，但是不能更改
+    char multiple[] = "my test String";
+
+    char* p = &multiple[0];
+
+    printf("the p for [0] is %p \n", p);
+
+    p = multiple;
+
+    printf("the p for multiple is %p \n", p);
+
+    printf("the second char is %c \n ",  *(multiple + 1));
+
+
+    // 可以改变地址但是不能改变内容的指针
+//    const char *p_const1 = &multiple[0];
+
+//    p_const1 = &multiple[3]; // can change addr
+
+//    multiple = "123"; // can not change content
+
+
+
+
+    // 可以改变内容但是不能改变地址的指针
+    char *const p_const2 = multiple;
+
+
+//    p_const2 = &multiple[3]; // can not change addr
+
+//    multiple = "my test String";
+
+
+
+
+
 
 
 }
