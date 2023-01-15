bfe86643e8952095d50de6ed49ebb788ad421492
ShuoWen5656 <2260755767@qq.com>
2023-01-15 19:18:19 +0800

    [新增代码]1、栈结构方法定义完成

--- Common/data_struct/stack/stack.c
+++ Common/data_struct/stack/stack.c
@@ -2,3 +2,10 @@
 // Created by 赵烁文 on 2023/1/15.
 //
 
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
--- Common/data_struct/stack/stack.h
+++ Common/data_struct/stack/stack.h
@@ -5,6 +5,10 @@
 #ifndef MYCPROJECT_STACK_H
 #define MYCPROJECT_STACK_H
 
+
+#include "../arraylist/arraylist.h"
+#include "stdbool.h"
+
 #endif //MYCPROJECT_STACK_H
 /*****************************************************************************************************
  * 由于C++编译器支持重载，函数名称会做一些处理，而在c中仅是简单函数名，这里定义为了告诉c++编译器按照C语言的方式编译即可*
@@ -17,6 +21,64 @@ extern "C"
 /**
  * 类似于java Stack功能
  */
+
+// 栈结构定义,相当于arraylist的一层封装
+typedef struct Stack {
+    ArrayList* arrayList;
+}Stack;
+
+
+/**
+ * 初始化栈结构
+ * @return
+ */
+Stack* stack_init();
+
+/**
+ * 加入一个元素
+ * @param stack
+ * @param p_data
+ * @return
+ */
+int stack_push(Stack* stack, void* p_data);
+
+/**
+ * 弹出一个元素
+ * @param stack
+ * @return
+ */
+int stack_poll(Stack* stack);
+
+/**
+ * 判断栈是否为空
+ * @param stack
+ * @return
+ */
+int stack_is_empty(Stack* stack);
+
+/**
+ * 获取栈顶元素
+ * @param stack
+ * @return
+ */
+int stack_peek(Stack* stack);
+
+/**
+ * 清空栈
+ * @param stack
+ */
+void stack_clear(Stack* stack);
+
+/**
+ * 释放栈内存
+ * @param stack
+ */
+void stack_free(Stack* stack);
+
+
+
+
+
 
 
 
